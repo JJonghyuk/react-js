05ac9c468c3b25adaba984b1cf3ca4c6c95591cb
JJonghyuk <kilerjong@naver.com>
2025-06-04 18:28:23 +0900

    [25.06.04] 리엑트-마스터즈 - ~5.8

--- react-study-info/5-Crypto Tracker/5.7/index.tsx
+++ react-study-info/5-Crypto Tracker/5.7/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { theme } from "./theme";
-
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -13,10 +10,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
