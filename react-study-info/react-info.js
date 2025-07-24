3c8263829346dea1ed642cb5e8da4c3249a817b1
JJonghyuk <kilerjong@naver.com>
2025-07-24 18:05:24 +0900

    [25.07.24] 리엑트-마스터즈 - ~ 9.2

--- react-study-info/react-info.js
+++ react-study-info/react-info.js
@@ -1315,3 +1315,36 @@
 // #8.16
 
 // ----------------------------- //#8 Animations -----------------------------
+
+// ----------------------------- #9 Nomflix Clone -----------------------------
+
+// #9.02
+// [2022.02.17]
+// **react-router-dom v5 vs v6**
+
+// 1. Link에서 to는 상대경로로 적으시면 됩니다
+// ex. '/tv' -> 'tv'
+
+// 2. exact가 사라졌습니다
+// 대신 알아서 최적의 경로를 react-router-dom이 판단하여 매칭해줍니다
+
+// 3. useRouteMatch가 useMatch로 변경되었습니다
+// 이 또한 상대경로로 작성하는 것으로 변경되었습니다
+// ex. useRouteMatch('/tv') -> useMatch('tv')
+
+// https://reactrouter.com/docs/en/v6/upgrading/v5#upgrade-to-react-router-v6
+
+// - whileHover
+// 호버 제스처가 인식되는 동안 애니메이션할 속성 또는 variant label입니다.
+
+// #9.03
+
+// #9.04
+
+// #9.05
+
+// #9.06
+
+// #9.07
+
+// ----------------------------- //#9 Nomflix Clone -----------------------------
