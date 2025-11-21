81b1779e67c26b790e7dcf3800c9e422a4824432
JJonghyuk <70563540+JJonghyuk@users.noreply.github.com>
2025-11-21 09:13:59 +0900

    Update App.js

--- react-study-info/3-Typescript/3.1 /App.js
+++ react-study-info/3-Typescript/3.1 /App.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+// # 3.1
+// 타입스크립트 설치
+// npx create-react-app 내 앱 이름 --template typescript
+// npm i --save-dev @types/styled-components
+// ----> @types/ 라는걸 추가하여 type스크립트가 적용 되어진거
+// npm i styled-components
+// npm i styled-components --legacy-peer-deps
+
 function App() {
   return;
 }
