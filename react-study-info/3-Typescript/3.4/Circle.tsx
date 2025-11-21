4ac6fff19daf52e285697a75d8a94e0cb0d9f587
JJonghyuk <70563540+JJonghyuk@users.noreply.github.com>
2025-11-21 10:17:09 +0900

    Update Circle.tsx

--- react-study-info/3-Typescript/3.4/Circle.tsx
+++ react-study-info/3-Typescript/3.4/Circle.tsx
@@ -19,6 +19,18 @@ interface CircleProps {
   borderColor?: string;
 }
 
+// # 3.4
+// TypeScript에서는 useState() 안에 들어가는 숫자, 스트링, 블린(boolean) 타입을 자동으로 읽어서 배치한다.
+// const [value, setValue] = useState(3); --> number로 자동 배치
+// setValue(3)
+// const [value, setValue] = useState<number|string>(); --> <number|string> 넣을 시에 둘다 사용 가능
+// setValue(3), setValue("hello")
+
+// useState < number > ( )
+// state의 type을 지정하려면 Generics안에 타입을 지정
+// 일반적으로는 초기값을 지정하면 타입스크립트가 자동으로 타입을 유추하기 때문에 굳이 지정해주지 않아도 되지만 상태가 undefined또는 null이 될 수도 있거나 객체 또는 배열일 때는 지정해주는 것이 좋다.
+// ex) const [ value, setValue ] = useState< Value | null >(null);
+
 function Circle({ bgColor, borderColor }: CircleProps) {
   // const [value, setValue] = useState(3);
   // setValue(6666);
